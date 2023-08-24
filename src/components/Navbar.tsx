1c5afefcd787092f7f988f55b39d4f0f86b58a21
Ogaga Iyara <iyaraogaga69@gmail.com>
2023-08-24 08:57:17 +0100

    Add navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,36 +1,48 @@
 import { FC } from "react";
 import logo from "./../images/pundit_logo (1).png";
 import { Link } from "react-scroll";
+import styles from "./../sass/_navbar.module.scss";
 
 const Navbar: FC = () => {
   return (
-    <div>
-      <Link spy={true} smooth={true} duration={500} to="hero">
-        <img src={logo} alt="Pundit Auto logo" />
+    <div className={styles.navigation}>
+      <Link
+        className={styles["navigation__title"]}
+        spy={true}
+        smooth={true}
+        duration={500}
+        to="hero"
+      >
+        <img
+          className={styles["navigation__image"]}
+          src={logo}
+          alt="Pundit Auto logo"
+        />
+        <p>Pundit Auto Mobile Nigeria</p>
       </Link>
-      <nav>
-        <ul>
-          <li>
+      <nav className={styles["navigation__nav"]}>
+        <ul className={styles["navigation__list"]}>
+          <li className={styles.navlink}>
             <Link spy={true} smooth={true} duration={500} to="hero">
               Home
             </Link>
           </li>
-          <li>
+          <li className={styles.navlink}>
             <Link spy={true} smooth={true} duration={500} to="about">
               About
             </Link>
           </li>
-          <li>
+          <li className={styles.navlink}>
             <Link spy={true} smooth={true} duration={500} to="services">
               Services
             </Link>
           </li>
-          <li>
+          <li className={styles.navlink}>
             <Link spy={true} smooth={true} duration={500} to="gallery">
               Gallery
             </Link>
           </li>
-          <li>
+          <li className={styles.navlink}>
             <Link spy={true} smooth={true} duration={500} to="contact">
               Contact
             </Link>
