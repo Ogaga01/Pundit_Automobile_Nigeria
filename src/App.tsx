99b2845073ffc457e28d73c8f83f01dd7c983fef
Ogaga Iyara <iyaraogaga69@gmail.com>
2023-08-24 11:11:52 +0100

    Add hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
 import Navbar from "./components/Navbar";
 import MobileNavbar from "./components/MobileNavbar";
+import Hero from "./components/Hero";
 
 const App: FC = () => {
   return (
     <div>
       <Navbar />
       <MobileNavbar />
+      <Hero />
     </div>
   );
 };
---
+++ src/components/Hero.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react";
+import styles from "./../sass/_hero.module.scss";
+
+const Hero: FC = () => {
+  return (
+    <section id="hero" className={styles["hero"]}>
+      <div className={styles["hero__div"]}>
+        <h1 className={styles["hero__div--head1"]}>
+          Pundit Auto mobile Nigeria
+        </h1>
+        <h2 className={styles["hero__div--head2"]}>
+          Your trusted specialists for a professional diagnosis, maintainance,
+          repairs, sales and after sales vehicle quality control.
+        </h2>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
