86e36e96aa815a8e7eca92679e6d0a3ba63d9b68
Ogaga Iyara <iyaraogaga69@gmail.com>
2023-08-28 07:41:53 +0100

    Add logo to hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,14 @@
 import { FC } from "react";
 import styles from "./../sass/_hero.module.scss";
+import logo from "./../images/pundit_logo (1).png";
 
 const Hero: FC = () => {
   return (
     <section id="hero" className={styles["hero"]}>
       <div className={styles["hero__div"]}>
+        <div className={styles["hero__div--img"]}>
+          <img className={styles["hero__div--image"]} src={logo} alt={logo} />
+        </div>
         <h1 className={styles["hero__div--head1"]}>
           Pundit Auto-mobile Nigeria
         </h1>
